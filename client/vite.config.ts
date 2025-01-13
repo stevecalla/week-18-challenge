5f99626a792d8f81369e42bb55fe6ec078f4b9fd
Steve Calla <callasteven@gmail.com>
2025-01-13 11:03:11 -0700

    Adjust

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this matches your `server.js` configuration
+  },
   server: {
     port: 3000,
     open: true,
@@ -13,6 +16,6 @@ export default defineConfig({
         secure: false,
         changeOrigin: true
       }
-    }
+    },
   }
 })
