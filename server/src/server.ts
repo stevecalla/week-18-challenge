e92cbf9c780dd70a13484b7dc850acb1d5ba7df8
Steve Calla <callasteven@gmail.com>
2025-01-13 12:09:14 -0700

    Update

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,8 +11,8 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.join(__dirname, '../client/build')));
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // app.use(express.static(path.join(__dirname, '../client/dist')));
   // app.use(express.static('../client/dist/index.html'));
 }
 
