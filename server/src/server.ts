d8b2b52f51975060e9c346c3a78ef458aad0e126
Steve Calla <callasteven@gmail.com>
2025-01-13 16:18:48 -0700

    Adjust

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,17 @@ import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
+import { fileURLToPath } from 'url';
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Replicate __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
